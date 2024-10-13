675a21c2a47c235530a1adb0d50a9742c79902b1
Pilot60 <98869539+Pilot60@users.noreply.github.com>
2024-10-13 19:12:49 -0400

    Add files via upload

---
+++ WeatherPROAPI/Geopy_Locator.py
@@ -0,0 +1,10 @@
+#from geopy import Nominatim
+#from WeatherAPI import *
+'''
+
+
+def WeatherLocation():
+    geolocator = Nominatim(user_agent="WeatherAPI")
+    User_location = geolocator.geocode(City_inp)
+    return User_location
+'''
