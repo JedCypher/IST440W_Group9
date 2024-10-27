6366cea09c5a049a721541acebf0accf88ce0453
Pilot60 <98869539+Pilot60@users.noreply.github.com>
2024-10-27 19:47:33 -0400

    Delete WeatherPROSandbox directory

--- WeatherPROSandbox/ApplicationStartUp.ps1
+++
@@ -1,6 +0,0 @@
-Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser -Force
-cd "$env:USERPROFILE\PycharmProjects\WeatherPROSandbox"
-.\.venv\Scripts\Activate
-Start-Process powershell "python -m http.server --cgi 8090"
-Start-Process "msedge.exe" "http://localhost:8090/Weathepro.html", "--inprivate"
-
