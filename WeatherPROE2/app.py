2c88c3b1ae25500921a62bd4238b0004c289dd65
Pilot60 <98869539+Pilot60@users.noreply.github.com>
2024-10-27 20:37:31 -0400

    Update app.py

--- WeatherPROE2/app.py
+++ WeatherPROE2/app.py
@@ -2,7 +2,7 @@ import os
 import requests
 import json
 from flask import Flask, render_template, request, jsonify
-from API_KEYS import api_key  
+from API_KEYS import api_key 
 
 app = Flask(__name__)
 application = app
@@ -17,20 +17,22 @@ def index():
 @app.route('/get_weather', methods=['post'])
 def get_weather():
     city = request.form.get('city')
-    state = request.form.get('state')
+    state_in = request.form.get('state')
     zip_code = request.form.get('zip')
 
 
-    response1 = requests.get(f"http://api.openweathermap.org/geo/1.0/direct?q={city},{state},{zip_code}&limit=1&appid={api_key}")
+    response1 = requests.get(f"http://api.openweathermap.org/geo/1.0/direct?q={city},{state_in},{zip_code}&limit=1&appid={api_key}")
     data2 = response1.json()
 
 
     if data2:
-        lat = data2[0]['lat']
-        lon = data2[0]['lon']
+        User_location1 = data2[0]['lat']
+        User_location2 = data2[0]['lon']
 
 
-        weather_response = requests.get(f"https://api.openweathermap.org/data/3.0/onecall?units=imperial&lat={lat}&lon={lon}&appid={api_key}")
+
+    # response = requests.get(f"https://api.openweathermap.org/data/2.5/weather?units=imperial&lat={User_location1}&lon={User_location2}&appid={api_key}")
+        weather_response = requests.get(f"https://api.openweathermap.org/data/3.0/onecall?units=imperial&lat={User_location1}&lon={User_location2}&appid={api_key}")
         weather_data = weather_response.json()
 
         combined_data = {
